b1b248f3431e17a41843f47b10a955386af1b70d
Fabio de Assis <assisfabio@gmail.com>
2019-06-15 16:33:07 -0300

    📱Arquivos para PWA

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
+const manifest = require('./public/manifest.json')
 const configureAPI = require('./api')
 
 module.exports = {
   publicPath: '/frontend-challenge',
   devServer: {
     before: configureAPI
+  },
+  pwa: {
+    name: manifest.name,
+    themeColor: manifest.theme_color,
+    msTileColor: manifest.background_color,
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: manifest.background_color
   }
 }
